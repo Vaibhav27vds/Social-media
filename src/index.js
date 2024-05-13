7d2fb49f3d7e710ead6c69b0be8e7b0f4b8a4ec3
Vaibhav Deep Srivastava <Vaibhav27vds@gmail.com>
2024-05-13 20:36:51 +0530

    connect to mongodb atlas

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import connectDB from "./db/index.js";
+import dotenv from "dotenv";
+
+dotenv.config({
+    path: './env'
+}) 
+
+// import express from express;
+
+// const app = express();
+
+// const port = process.env.PORT || 3000;
+
+// import express from express;
+
+// const app = express();
+// ;( async () => {
+//     try {
+//       await  mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//       app.on("error", (error) => {
+//         console.log("ERROR", error);
+//         throw error
+//       })
+
+//       app.listen (port, () => {
+//         console.log(`App is listening on port https://http://localhost:${port}`);
+//       })
+//     } catch (error) {
+//         console.error("ERROR", error)
+//     }
+// })()
+
+// this is the first approach but this has added alot of code to the index.js file so the other appproach to this will be that we will make a new file for the connection of the database write a similar code there and then we will import that function here in the index.js file.
+
+connectDB();
