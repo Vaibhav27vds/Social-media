871c91d8391ff40a8082aef815719fbd38666561
Vaibhav Deep Srivastava <Vaibhav27vds@gmail.com>
2024-05-26 04:07:06 +0530

    registering conreoller issue resolved

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -8,7 +8,7 @@ const registerUser = asyncHandler( async (req,res) => {
      const {
         fullname,
         email,
-        usename,
+        username,
         password
      } = req.body
      console.log("email: ", email);
@@ -19,7 +19,7 @@ const registerUser = asyncHandler( async (req,res) => {
         throw new ApiError(400, "All fields are required")
     }
 
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or: [{ username } , { email }]
     })
 
@@ -28,7 +28,14 @@ const registerUser = asyncHandler( async (req,res) => {
     }
 
     const avatarLocalPath = req.files?.avatar[0]?.path
-    const coverImageLocalPath = req.files?.coverImage[0]?.path
+    // const coverImageLocalPath = req.files?.coverImage[0]?.path
+
+
+    let coverImageLocalPath;
+    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > [0]){
+        coverImageLocalPath = req.files.coverImage[0].path
+    }
+
     if(!avatarLocalPath){
         throw new ApiError(400, "Avatar file  is required");
     }
@@ -50,7 +57,7 @@ const registerUser = asyncHandler( async (req,res) => {
     })
 
     const createdUser = await User.findById(user._id).select(
-        "-password -refreshtoken"
+        "-password -refreshtokens"
     )
 
     if(!createdUser){
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -31,7 +31,6 @@ const userSchema = new Schema({
     },
     coverImage: {
         type: String, // cloudinary url
-        required: true,
     },
     watchHistory: [
         {
